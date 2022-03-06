2c1aced0da29989d3a7d7de4946fffdaef33f788
Gökhan Can <55600314+chimpdev@users.noreply.github.com>
2022-03-06 23:01:48 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+function sendRequest() {
+  const token = document.querySelector('#token').value;
+  const newUsername = document.querySelector('#username').value;
+  if(newUsername.trim() == '') {
+    $('#change').css('border-color', 'red').css('color', 'red');
+    return $('.error').css('color', 'red').html('Username cannot be empty.');
+  };
+
+  $('.error').css('color', '#333').html('Your new username request is being processed.');
+  $('#change').removeAttr('onclick');
+  $.ajax({ 
+    url: 'https://discord.com/api/v9/users/@me',
+    method: 'PATCH',
+    headers: {
+      'Authorization': `Bot ${token}`,
+      'Content-Type': 'application/json'
+    },
+    data: JSON.stringify({ username: newUsername }),
+    success: data => {
+      $('#change').css('border-color', 'green').css('color', 'green');
+      $('.error').css('color', 'green').html('Your bot username has been changed.');
+    },
+    error: err => {
+      $('#change').css('border-color', 'red').css('color', 'red').attr('onclick', 'sendRequest()');
+      if(err.responseJSON.message == '401: Unauthorized') {
+        $('.error').css('color', 'red').html('Invalid token.');
+      } else $('.error').css('color', 'red').html(err.responseJSON.errors.username._errors[0].message);
+    }
+  });
+};
